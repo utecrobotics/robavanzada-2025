294cbb6dccca720aaaed643c01300ad887ab7825
Alexander López <lopez.manrique.alexander@gmail.com>
2025-10-03 14:03:16 -0500

    Update setup.py

--- avlab4/setup.py
+++ avlab4/setup.py
@@ -10,6 +10,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
